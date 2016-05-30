60a971986ac3c6eaa71460dd25d82eceedf1558a
hzaskywalker <hza@Yavin-2.local>
2016-05-30 23:03:19 +0800

    there is bug in service...

--- misc/cache/shareinfo.h
+++ misc/cache/shareinfo.h
@@ -7,6 +7,6 @@
 #define MAX_MEM_POS 30000
 #define MAX_CACHE_LEN 32
 #define MAX_BUS_LEN 32
-#define MEM_NAME (ftok("/Users/hza/homework/Makefile",44))
-#define BUS_NAME (ftok("/Users/hza/homework/Makefile", 43))
-#define MSG_NAME(x) (ftok("/Users/hza/homework/Makefile", x))
+#define MEM_NAME (ftok("Makefile",44))
+#define BUS_NAME (ftok("Makefile", 43))
+#define MSG_NAME(x) (ftok("Makefile", x))
